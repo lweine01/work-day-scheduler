fc3bb5b842a76f9124fb7021e54b847b04cc749b
lweine01 <laurarweiner@gmail.com>
2021-08-27 17:17:51 -0400

    Updated save button function to save appropriate key and value pairs. Started localStorage.getItem fnction.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
 var container = $(".container");
-// var currentTime = moment().format("H");
-var currentTime = 11;
+var currentTime = moment().format("H");
 var saveButton = $(".saveBtn");
 var timeBlock = $(".time-block");
-var inputForm = $("textarea");
+
 
 $("#currentDay").text(moment().format("dddd MMMM DD, YYYY"));
 
 timeBlock.each(function(){
     var el=$(this);
+    //console.log(typeof(el));
     var newClass = getClass(el);
-    el.addClass(newClass);
+    el.children().eq(1).addClass(newClass);
+   // console.log('showing elfunction', a);
 });
 
 function getClass(element){
     var idSelector = element.attr("id");
+    console.log(idSelector);
     if (parseInt(idSelector) === parseInt(currentTime)){
         return "present";
     } else if (parseInt(idSelector) < parseInt(currentTime)) {
@@ -24,11 +26,13 @@ function getClass(element){
     }
 }
 
-saveButton.on("click", function (event) {
-    event.preventDefault();
-     localStorage.setItem("savedPlan", inputForm.val);
+saveButton.on("click", function () {
+    var textValue = $(this).siblings(".description").val();
+    var idSelector = $(this).parent().attr("id");
+    localStorage.setItem(idSelector, textValue);
+    console.log(textValue);
 });
 
-console.log(typeof inputForm.val);
-console.log(inputForm);
-
+for (var i=9; i <8; i++){
+$("#" + i + " .description").val(localStorage.getItem(i));
+}
